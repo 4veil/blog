000c7b53b008e02227e414e6119cd01ed7b91f0b
Dan Brownlow <dan.brownlow@gmail.com>
2013-05-27 14:43:30 +0100

    Redesigned the home page

--- index.php
+++ index.php
@@ -5,36 +5,60 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div id="form">
-		<?php if(isset($_GET['id'])) { ?>
-		<h2>Single Post</h2>
+	
+	<div class="wrapper">
+		<div class="header"><p>DANISHERE</p></div>
+		<div class="navigation">
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+			</ul>
 
-		<?php $post = getSinglePost($_GET['id']); ?>
-			<fieldset>
-				<h2><?php echo $post['title']; ?> </h2>
-				<p><?php echo $post['content']; ?></p>
-			</fieldset>
+		</div>
 
-			<a href="index.php">Back to posts</a>
-		<?php } else { ?>
-		
-		<h2>All Posts</h2>
-		<?php $posts = getAllPosts(); ?>
+		<div class="content">
+			
+			<div id="form">
+				<?php if(isset($_GET['id'])) { ?>
+				<h2>Single Post</h2>
 
-		<?php 
-			foreach ($posts as $name => $post) {
-				echo "<h2>" . $post['title'] . "</h2>";
-				echo "<p>" . $post['content'] . "</p>";
+				<?php $post = getSinglePost($_GET['id']); ?>
+					<fieldset>
+						<h2><?php echo $post['title']; ?> </h2>
+						<p><?php echo $post['content']; ?></p>
+					</fieldset>
 
-				echo '<a href="index.php?id=' . $post['id'] . '" style="text-align:right;">View Post</a>';
-				echo "<hr>";
-			}
+					<a href="index.php">Back to posts</a>
+				<?php } else { ?>
+				
+				<h2>All Posts</h2>
+				<?php $posts = getAllPosts(); ?>
 
+				<?php 
+					foreach ($posts as $name => $post) {
+						echo "<h2>" . $post['title'] . "</h2>";
+						echo "<p>" . $post['content'] . "</p>";
 
-		?>
+						echo '<a href="index.php?id=' . $post['id'] . '" style="text-align:right;">View Post</a>';
+						echo "<hr>";
+					}
+				?>
 
-		<?php } ?>
+				<?php } ?>
+			</div>
+
+		</div>
+
+		<div class="sidebar">
+			<p>This is where the posts will go!</p>
+		</div>
+		<div class="footer">
+			<p>This is foot, hello.</p>
+		</div>
+
 	</div>
+
+	
 
 
 </body>
